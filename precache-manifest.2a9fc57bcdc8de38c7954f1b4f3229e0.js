846f2ff9b7be02230cb7d3c8eb33a4d0f17b531e
jeffrey-arias <jeffrey.e.arias@gmail.com>
2021-12-06 00:41:51 +0800

    Updates

---
+++ precache-manifest.2a9fc57bcdc8de38c7954f1b4f3229e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d78f94a4df50a9f92605458b145bc2",
+    "url": "/my-new-portfolio/index.html"
+  },
+  {
+    "revision": "843391a9c9a24238ff6a",
+    "url": "/my-new-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "9d6fb9955fe78c04847c",
+    "url": "/my-new-portfolio/static/css/main.7c857806.chunk.css"
+  },
+  {
+    "revision": "843391a9c9a24238ff6a",
+    "url": "/my-new-portfolio/static/js/2.ef577863.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my-new-portfolio/static/js/2.ef577863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6fb9955fe78c04847c",
+    "url": "/my-new-portfolio/static/js/main.f24b02e6.chunk.js"
+  },
+  {
+    "revision": "56fa98d070ef2874843d",
+    "url": "/my-new-portfolio/static/js/runtime-main.0ef5ebe5.js"
+  }
+]);
