fc6937a85f8beb32547f095a4f65e1c1836e9e61
jeffrey-arias <jeffrey.e.arias@gmail.com>
2024-08-05 22:06:44 +0800

    feat: update dependencies

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Typical from "react-typical";
 import Switch from "react-switch";
 
 class Header extends Component {
@@ -27,32 +26,41 @@ class Header extends Component {
   render() {
     if (this.props.sharedData) {
       var name = this.props.sharedData.name;
-      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ] ).flat();
+      this.titles = this.props.sharedData.titles
+        .map((x) => [x.toUpperCase(), 1500])
+        .flat();
     }
 
-    const HeaderTitleTypeAnimation = React.memo( () => {
-      return <Typical className="title-styles" steps={this.titles} loop={50} />
-    }, (props, prevProp) => true);
-
     return (
-      <header id="home" style={{ height: window.innerHeight - 220, display: 'block', border: 0 }}>
-        <div className="row aligner" style={{height: '100%'}}>
+      <header
+        id="home"
+        style={{
+          height: window.innerHeight - 220,
+          display: "block",
+          border: 0,
+        }}
+      >
+        <div className="row aligner" style={{ height: "100%" }}>
           <div className="col-md-12">
             <div>
-              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
-              <br/>
+              <span
+                className="iconify header-icon"
+                data-icon="la:laptop-code"
+                data-inline="false"
+              ></span>
+              <br />
               <h1 className="mb-0">
-                <Typical steps={[name]} wrapper="p" />
+                <p>{name}</p>
               </h1>
-              <div className="title-container">
-                <HeaderTitleTypeAnimation />
+              <div className="title-container" style={{ marginBottom: 100 }}>
+                <p className="title-styles">WEB & MOBILE DEVELOPER</p>
               </div>
               <Switch
                 checked={this.state.checked}
                 onChange={this.onThemeSwitchChange}
                 offColor="#339acc"
                 onColor="#353535"
-                className="react-switch mx-auto"
+                className="react-switch mx-auto switch"
                 width={90}
                 height={30}
                 uncheckedIcon={
